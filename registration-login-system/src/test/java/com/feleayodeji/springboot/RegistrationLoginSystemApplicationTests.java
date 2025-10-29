faa1fae8ef225a046b593516b1307683e5685f23
FeleAyodeji <feledeji@gmail.com>
2025-10-29 02:43:35 +0100

    first commit

---
+++ registration-login-system/src/test/java/com/feleayodeji/springboot/RegistrationLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feleayodeji.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
